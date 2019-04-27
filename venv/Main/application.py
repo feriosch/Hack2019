1cd129ea05cae26d40e442321b80c381ddfe90da
feriosch <47795404+feriosch@users.noreply.github.com>
2019-04-27 15:56:25 -0500

    Initial commit

---
+++ venv/Main/application.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template,request, redirect, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.secret_key = "ElPatatita"
+
+
+
+@app.route('/', methods=['GET','POST'])
+def home_page():
+    if request.method == 'POST':
+        request.form.get("palabra_clave")
+        
